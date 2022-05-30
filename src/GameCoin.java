e47db76b1ab3febbbd168e8a18c180e58cb37cd3
glizondo <80534057+glizondo@users.noreply.github.com>
2022-05-30 14:19:51 -0400

    Multiple coin toss feature implemented with counter on screen

--- src/GameCoin.java
+++ src/GameCoin.java
@@ -18,6 +18,8 @@ public class GameCoin {
 	private JFrame frame;
 	private Coin coin;
 	private JTextField textFieldResult;
+	private JTextField textFieldCount;
+	private int countToss;
 
 	/**
 	 * Launch the application.
@@ -74,7 +76,8 @@ public class GameCoin {
 					System.out.println("Something went wrong with the coin toss");
 
 				}
-
+				countToss++;
+				textFieldCount.setText(Integer.toString(countToss));
 			}
 		});
 		buttonTossCoin.setBounds(229, 69, 138, 63);
@@ -86,6 +89,20 @@ public class GameCoin {
 		textFieldResult.setBounds(10, 283, 564, 67);
 		frame.getContentPane().add(textFieldResult);
 		textFieldResult.setColumns(10);
+
+		JLabel labelCount = new JLabel("Count");
+		labelCount.setBackground(Color.GRAY);
+		labelCount.setOpaque(true);
+		labelCount.setFont(new Font("Stencil", Font.PLAIN, 12));
+		labelCount.setHorizontalAlignment(SwingConstants.CENTER);
+		labelCount.setBounds(10, 69, 109, 24);
+		frame.getContentPane().add(labelCount);
+
+		textFieldCount = new JTextField();
+		textFieldCount.setEditable(false);
+		textFieldCount.setBounds(129, 69, 65, 24);
+		frame.getContentPane().add(textFieldCount);
+		textFieldCount.setColumns(10);
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
